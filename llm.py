d598b3eb6ae90c7e91b97521fd29fcf689f2ed56
Microsoft Graph DevX Tooling <timwamalwa@gmail.com>
2024-08-24 23:29:41 +0300

    Completed first tutorial about lanchain

--- llm.py
+++ llm.py
@@ -3,6 +3,10 @@ from langchain_openai import ChatOpenAI
 openAiKey = os.environ.get("OPENAI_API_KEY")
 llm = ChatOpenAI(
     api_key=openAiKey,
+    model="gpt-3.5-turbo",
+    temperature=0.7,
 )
-response = llm.invoke("Hello, how are you?")
-print(response)
+response = llm.batch(["Hello, how are you?", "What is the meaning of life?"])
+
+for chunk in response:
+    print(chunk.content, end="", flush=True)
